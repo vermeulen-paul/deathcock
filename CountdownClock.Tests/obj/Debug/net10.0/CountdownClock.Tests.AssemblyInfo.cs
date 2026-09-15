a4520b167b424abf3f0b77550c6c3921f3627b83
Paul Vermeulen <pv2@cowhills.com>
2026-09-15 11:43:40 +0200

    .

--- CountdownClock.Tests/obj/Debug/net10.0/CountdownClock.Tests.AssemblyInfo.cs
+++ CountdownClock.Tests/obj/Debug/net10.0/CountdownClock.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CountdownClock.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3ca3863e30653b494d093e3527c0c6bef13ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CountdownClock.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CountdownClock.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
